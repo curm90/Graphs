4855aed8c972bf32753cd5d1bcbfb2037d504f00
Liam Sutton <devliam90@gmail.com>
2020-03-17 15:24:40 +0000

    add create graph function

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -15,5 +15,22 @@ def Queue():
         return len(self.queue)
 
 
+def create_graph(ancestors):
+    graph = {}
+
+    for pair in ancestors:
+        key = pair[1]
+        if key not in graph:
+            graph[key] = set()
+            graph[key].add(pair[0])
+        else:
+            graph[key].add(pair[0])
+    return graph
+
+
+print(create_graph([(1, 3), (2, 3), (3, 6), (5, 6),
+                    (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]))
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
